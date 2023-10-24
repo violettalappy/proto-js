a07dc0cbaada501ce8c878bcb05e2f0707d1bcaa
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-10-24 10:25:43 +0700

    Update sketch.js

--- demo-p5sound/sketch.js
+++ demo-p5sound/sketch.js
@@ -19,17 +19,28 @@ function preload(){
 function setup() {
   createCanvas(200, 200);
   background(random(255));
+
   SliderVolume = createSlider(0, 1, 0.5, 0.01);
   SliderPitch = createSlider(0, 5, 1, 0.01);
   SliderPan = createSlider(-1, 1, 0, 0.01);
+
   PlayButton = createButton("play");
   PlayButton.mousePressed(TogglePlayback);
+
   DefaultButton = createButton("default");
+  DefaultButton.mousePressed(ToggleDefault);
+
   PanicButton = createButton("panic!!");
-  MutateButton = createButton("mutate");
+  PanicButton.mousePressed(TogglePanic);
+
+  MutateButton = createButton("mutate");  
 }
 
 function draw() {
+  UpdateSong()
+}
+
+function UpdateSong(){
   song.setVolume(SliderVolume.value());
   song.rate(SliderPitch.value());
   song.pan(SliderPan.value());
@@ -42,4 +53,19 @@ function TogglePlayback(){
   else{
     song.pause();
   }
+}
+
+function ToggleDefault(){  
+  SliderVolume.value(0.5);
+  SliderPitch.value(1);
+  SliderPan.value(0);
+  UpdateSong();
+}
+
+function TogglePanic(){
+  SliderVolume.value(0.5);
+  SliderPitch.value(1);
+  SliderPan.value(0);
+  UpdateSong();
+  song.stop();
 }
