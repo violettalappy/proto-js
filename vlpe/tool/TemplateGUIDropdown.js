097b9d102ac3111ef995fbaef574d3bb0b414de8
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-11-22 02:17:55 +0700

    Update TemplateGUIDropdown.js

--- vlpe/tool/TemplateGUIDropdown.js
+++ vlpe/tool/TemplateGUIDropdown.js
@@ -20,6 +20,13 @@ const _sizes = [
     new Template(0, "Difficulty2", ["Easy", "Normal", "Hard"]),
     new Template(0, "Difficulty3", ["Very Easy", "Easy", "Normal", "Hard", "Very Hard"]),
     new Template(0, "Difficulty4", ["Novice", "Easy", "Normal", "Hard", "Chaos", "Expert"]),
+    new Template(0, "DifficultyTouhou", ["Baby"]),
+    new Template(0, "DifficultyWarriorOrochi", ["Novice", "Easy", "Normal", "Hard", "Chaos", "Expert"]),
+    new Template(0, "DifficultyDoom", ["Don't hurt me mommy"]),
+    new Template(0, "DifficultyQuake", ["Something"]),
+    
+    new Template(0, "DifficultyWolfenstein", ["Can I play, Daddy?", "Don't hurt me", "Bring 'em on!", "Do or die!", "Call me Terror-Billy!", "I am death incarnate!", "Uber", "Mein leben"]),
+    
     new Template(0, "OptionsDisplayResolution", ["640x480", "1920x1080"]),
     new Template(0, "OptionsDisplayRefreshRate", ["24","30","36","48","60","72","75","90","120","144","165","240","360","480"]),
 ];
