d01a6f29d34438ea944b2e78a81a081ffdd11adf
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-12-14 21:46:18 +0700

    Update Stuffs

---
+++ src/vlpetool-textgen/src/ProgramConfig.js
@@ -0,0 +1,16 @@
+// The file in question will save to config.csv
+// Must display all possible options that user can make
+
+class Template {
+    constructor(){
+    }
+
+    GetText() {
+    }
+}
+
+_templates = [
+    new Template("Something Name", ["Something"]),
+    new Template("IDontKnow", ["Something", "Something"]),
+];
+
