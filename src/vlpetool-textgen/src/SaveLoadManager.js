467999e8ede18f588cbdecd65bffeabae00cb676
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-12-14 23:36:15 +0700

    Update SaveLoadManager.js

--- src/vlpetool-textgen/src/SaveLoadManager.js
+++ src/vlpetool-textgen/src/SaveLoadManager.js
@@ -2,18 +2,26 @@
 // SavePath set to SaveData file location, there should be a fallback just in case
 // SaveSpecific()
 // LoadSpecific()
-
+// OnCheckCompleted()
+// OnSaveSuccess()
+// OnSaveFailed()
 
 // ! EXTERNAL USE
 
+const K_FILE_EXTENSION = "csv";
 const K_FOLDER_PATH = "savedata";
 
 function Export_Unity(){
     let temp = "";
     // License
+    
     // Variable for save path
     temp = "string str_foldername = \"" + K_FOLDER_PATH + "\";";
-    // SaveData path
+    
+    
+
+    // End of file
+    temp += "};";
 }
 
 function Export_VLPE_JS(){
