536f6d713c20855dc724c1e4e3ab68b92c592ca5
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-12-14 22:58:52 +0700

    Create SaveLoadManager.js

---
+++ src/vlpetool-textgen/src/SaveLoadManager.js
@@ -0,0 +1,20 @@
+// FileExtension
+// SavePath set to SaveData file location, there should be a fallback just in case
+// SaveSpecific()
+// LoadSpecific()
+
+
+// ! EXTERNAL USE
+
+const K_FOLDER_PATH = "savedata";
+
+function Export_Unity(){
+    let temp = "";
+    // License
+    // Variable for save path
+    temp = "string str_foldername = \"" + K_FOLDER_PATH + "\";";
+    // SaveData path
+}
+
+function Export_VLPE_JS(){
+}
